2d3f87227541e6576a823f1dcb27e7a7c9eaaea5
githubdaAna <kristinnacruzz@gmail.com>
2023-03-27 12:17:10 -0300

    Ajustar layout page main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,9 +4,8 @@ import Button from '../../components/Button';
 const Main = () => {
     return (
         <div className="main-content">
-            <h1>Oi, eu sou a Ana, <br></br>desenvolvedora web fullstack.</h1>
-            <h2>Esse é meu website, onde você pode conhecer melhor meus projetos.</h2>
-            <Button>Download Currículo</Button>
+            <h1>Oi, <br></br>eu sou a Ana, <br></br>desenvolvedora web fullstack.</h1>
+            <Button>Download do Currículo</Button>
         </div >
     )
 }
